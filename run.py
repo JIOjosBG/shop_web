62abced3d7312268a2ac9188523de7c82eeceedd
JIOjosBG <jjjooosssiiifff@gmail.com>
2019-06-29 16:37:50 +0200

    database

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
 from flask import Flask , render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
-app = Flask('__name__')
 
+app = Flask('__name__')
 app.config['SECRET_KEY'] = '352503b0a28a1e79a92dde75ea02b971'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),nullable=False,unique=True)
+    email = db.Column(db.String(120),nullable=False,unique=True)
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
+    password = db.Column(db.String(60),nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
 
 itemlist=[
     {
