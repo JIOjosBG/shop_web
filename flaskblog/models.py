ed0dd647b8d2f447f42ddbda96e103ab30b0f0f3
JIOjosBG <jjjooosssiiifff@gmail.com>
2019-07-12 16:47:39 +0300

    user login logout one user add item

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,11 @@
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
-class Users(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20),nullable=False,unique=True)
     email = db.Column(db.String(120),nullable=False,unique=True)
@@ -8,7 +13,7 @@ class Users(db.Model):
     password = db.Column(db.String(60),nullable=False)
 
     def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
+        return f"User('{self.username}','{self.email}','{self.image_file}','{self.password}')"
 
 class Items(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template, flash, redirect, url_for
-from flaskblog import app, db
+from flask import render_template, flash, redirect, url_for, request
+from flaskblog import app, db, bcrypt
 from flaskblog.models import Users, Items
 from flaskblog.forms import RegistrationForm,LoginForm, NewItemForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 itemlist=Items.query.all()
+
 
 @app.route('/')
 @app.route('/home')
@@ -26,16 +28,17 @@ def register():
         username=form.username.data
         email=form.email.data
         password=form.password.data
-        if Users.query.filter_by(username=username).first()or Users.query.filter_by(email=email).first():
+        hashed_password=bcrypt.generate_password_hash(password).decode("utf-8")
+        if Users.query.filter_by(username=username).first() or Users.query.filter_by(email=email).first():
             flash(f'Could not create account.Username or email is already taken!','danger')
             return redirect(url_for('register'))
         else:
-            user=Users(username=username,password=password,email=email)
+            user=Users(username=username,password=hashed_password,email=email)
             db.session.add(user)
             db.session.commit()
             flash(f'Account created for {form.username.data}!','success')
             print(Users.query.all())
-            return redirect(url_for('home'))
+            return redirect(url_for('login'))
             
         flash(f'Account created for {form.username.data}!','success')
         return redirect(url_for('home'))
@@ -47,12 +50,14 @@ def login():
     form= LoginForm()
     email=form.email.data
     password=form.password.data
-    user=Users(email=email,password=password)
-
+    remember=form.remember.data
     if form.validate_on_submit():
-        if Users.query.filter_by(email=email,password=password).first():
-            flash('You have been logged in','success')
-            return redirect(url_for('home'))
+        print(Users.query.all())
+        user = Users.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password,password):
+            login_user(user, remember=remember)
+            next_page=request.args.get('next')
+            return  redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login unsuccessful','danger')
     return render_template('login.html',title='Login',form=form)
@@ -80,3 +85,13 @@ def newitem():
             return redirect(url_for('items'))
             
     return render_template('newitem.html',title='New Item',form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html',title='Account')
