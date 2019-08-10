83e12243eb39dd3a9d238862158bcae65325fe57
JIOjosBG <jjjooosssiiifff@gmail.com>
2019-08-10 17:16:13 +0300

    pagination on all items and specific items route

--- flaskblog/items/routes.py
+++ flaskblog/items/routes.py
@@ -5,13 +5,15 @@ from flaskblog.models import Items
 from flaskblog.items.forms import NewItemForm
 
 
-itemlist=Items.query.all()
 
 items = Blueprint('items',__name__)
 
 @items.route('/items')
 def allitems():
-    return render_template("items.html",title='items',items=itemlist)
+    page = request.args.get('page',1,type=int)
+    print(page)
+    show_items = Items.query.order_by(Items.id.desc()).paginate(page=page, per_page=10)
+    return render_template("items.html",title='items',items=show_items)
 
 
 @items.route('/newitem', methods=['GET','POST'])
@@ -37,3 +39,23 @@ def newitem():
             return redirect(url_for('items.allitems'))
 
     return render_template('newitem.html',title='New Item',form=form)
+
+
+
+@items.route('/specific_items/')
+def specific_items():
+    page = request.args.get('page',1,type=int)
+    item_type = request.args.get('type',"ranica",type=str)
+    print(page,item_type,"\n\n\n\n\n\n\n\n")
+    show_items = Items.query.filter_by(type=item_type)\
+    .order_by(Items.id.desc())\
+    .paginate(page=page, per_page=10)
+    return render_template("items.html",title='items',items=show_items)
+
+
+
+    #show_items = Items.query.filter_by(type=type)\
+    #.order_by(Items.id.desc())\
+    #.paginate(page=page, per_page=10)
+
+
